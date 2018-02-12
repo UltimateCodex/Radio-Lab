31da6fba259b49f63e7392be440c10ed9e8c025b
Sabrina Berger <sabrinaberger@berkeley.edu>
2018-02-11 18:29:56 -0800

    figuring out plotting scripts

---
+++ Lab_2/immediate_check.py
@@ -0,0 +1,71 @@
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Get files to FFT')
+
+parser.add_argument('-data_file', type=str, help='file to be FFTed')
+parser.add_argument('-N', type=int, help='divisor used for sample frequency')
+parser.add_argument('-dual', type=bool, help='dual mode or nah')
+parser.add_argument('-volt_range', type=str, help='maximum in voltage range')
+
+
+args = parser.parse_args()
+
+filename = args.data_file
+divisor = args.N
+dual = args.dual
+data_taken = args.data_file
+volt_range = args.volt_range
+
+def scaleToReal(analog_values, volt_range=[-1, 1]):
+    type_info = np.iinfo(np.int16)
+
+    x1 = volt_range[0]
+    x2 = volt_range[1]
+
+    a1 = type_info.min
+    a2 = type_info.max
+
+    real_values = np.zeros(len(analog_values), dtype=np.complex)
+
+    for i in range(len(analog_values)):
+        real_values[i] = (float(x2 - x1) * float(analog_values[i] - a1) / float(a2 - a1) + x1)
+    return np.asarray(real_values)
+
+
+# def scaleToReal(analog_values):
+#     return analog_values/16384
+
+def getPowerSpectra(input_voltage):
+    fourier = np.fft.fft(input_voltage)
+    freqs = np.fft.fftfreq(len(fourier), 1 / (v_samp)) / 1e6
+    power = np.abs(fourier) ** 2
+    return power, freqs
+
+if dual:
+    data = np.load(filename)
+    sample_size = int(len(data) / 2)
+
+    dual_imag = scaleToReal(np.load(filename), [-int(volt_range[:-2]), int(volt_range[:-2])])[int(sample_size):]
+    dual_real = scaleToReal(np.load(filename), [-int(volt_range[:-2]), int(volt_range[:-2])])[:int(sample_size)]
+
+    data = np.zeros(sample_size, dtype=np.complex)
+
+    for i in range(int(sample_size)):
+        data[i] = np.complex(dual_real[i], dual_imag[i])
+
+else:
+    data = np.load(filename)
+    sample_size = len(data)
+
+v_samp = 62.5e6 / divisor
+
+power, freqs = getPowerSpectra(data)
+
+plt.plot(np.fft.fftshift(freqs), np.fft.fftshift(power))
+plt.xlabel("Frequency (MHz)", fontsize=20)
+plt.ylabel("Power $(volt-second)^2$", fontsize=20)
+plt.show()
+
+
--- Lab_2/lab2_initial_check.py
+++ Lab_2/lab2_initial_check.py
@@ -1,19 +1,5 @@
-import argparse
-
 import matplotlib.pyplot as plt
 import numpy as np
-
-# parser = argparse.ArgumentParser(description='Get files to FFT')
-#
-# parser.add_argument('-data_file', type=str, help='file to be FFTed')
-# parser.add_argument('-N', type=int, help='divisor used for sample frequency')
-# parser.add_argument('-dual', type=bool, help='dual mode or nah')
-# parser.add_argument('-volt_range', type=int, help='maximum in voltage range')
-#
-# args = parser.parse_args()
-# N = parser.N
-# dual = parser.dual
-# data_taken = args.data_file
 
 
 def scaleToReal(analog_values, volt_range=[-1, 1]):
@@ -25,47 +11,63 @@ def scaleToReal(analog_values, volt_range=[-1, 1]):
     a1 = type_info.min
     a2 = type_info.max
 
-    real_values = []
-    real2_values = []
-    for i in analog_values:
-        real_values.append(float(x2 - x1) * float(i - a1) / float(a2 - a1) + x1)
+    real_values = np.zeros(len(analog_values), dtype=np.complex)
+
+    for i in range(len(analog_values)):
+        real_values[i] = (float(x2 - x1) * float(analog_values[i] - a1) / float(a2 - a1) + x1)
     return np.asarray(real_values)
 
 
+# def scaleToReal(analog_values):
+#     return analog_values/16384
+
 def getPowerSpectra(input_voltage):
-    power, freqs = np.abs(getFFT(input_voltage)) ** 2
-    return power
+    fourier = np.fft.fft(input_voltage)
+    freqs = np.fft.fftfreq(len(fourier), 1 / (v_samp)) / 1e6
+    power = np.abs(fourier) ** 2
+    return power, freqs
 
 
 def getFFT(input_voltage):
-    fourier = np.fft.fft(scaleToReal(input_voltage))
+    fourier = np.fft.fft(input_voltage)
     freqs = np.fft.fftfreq(len(fourier), 1 / (v_samp)) / 1e6
     return fourier, freqs
 
+
 if __name__ == '__main__':
-    N = 1
-    data_taken = "initial_test_100mV_1.npy"
-    dual = True
-    sample_size = len(data_taken)
+    N = [1, 2, 3]
+    volt_range = ['100mV']
 
-    if dual:
-        dual_imag = np.load(data_taken)[int(sample_size / 2):]
-        dual_real = np.load(data_taken)[:int(sample_size / 2)]
-        data = np.zeros((sample_size), dtype=np.complex)
-    for i in range(N):
-        data[i] = np.complex(dual_real[i], dual_imag[i])
-    else:
-        data = np.load(data_taken)
+    for divisor in N:
+        for volts in volt_range:
+            filename = "initial_test_" + volts + "_" + str(divisor) + ".npy"
+            dual = True
 
-    v_samp = 62.5e6 / N
+            if dual:
+                data = np.load(filename)
+                sample_size = int(len(data) / 2)
 
-    fourier, freqs = getFFT(data)
+                dual_imag = scaleToReal(np.load(filename), [-int(volts[:-2]), int(volts[:-2])])[int(sample_size):]
+                dual_real = scaleToReal(np.load(filename), [-int(volts[:-2]), int(volts[:-2])])[:int(sample_size)]
 
-    power = getPowerSpectra(data)
+                data = np.zeros(sample_size, dtype=np.complex)
 
-    plt.plot(np.fft.fftshift(freqs), np.fft.fftshift(power))
-    plt.xlabel("Frequency (MHz)", fontsize=20)
-    plt.ylabel("$(volt-second)$", fontsize=20)
-    plt.show()
+                for i in range(int(sample_size)):
+                    data[i] = np.complex(dual_real[i], dual_imag[i])
+
+            else:
+                data = np.load(filename)
+                sample_size = len(data)
+
+            v_samp = 62.5e6 / divisor
+
+            power, freqs = getPowerSpectra(data)
+            # power, freqs = getPowerSpectra(scaleToReal(data))
 
 
+            plt.plot(np.fft.fftshift(freqs), np.fft.fftshift(power))
+            plt.xlabel("Frequency (MHz)", fontsize=20)
+            plt.ylabel("Power $(volt-second)^2$", fontsize=20)
+            plt.title("Divisor = " + str(divisor) + ", Voltage Max = " + volts)
+            plt.xlim(-1, 1)
+            plt.show()
